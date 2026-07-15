7ce6af8da99c8c1ffb29e8719b0cabb15725ead7
Tinku785 <139141786+tinkukaushik@users.noreply.github.com>
2026-07-15 22:16:22 +0530

    feat: add search registry route with authentication middleware

--- email-portal/backend/src/routes/registryRoutes.js
+++ email-portal/backend/src/routes/registryRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const { searchRegistry } = require('../controllers/registryController');
 
 // NOTE: Once finishes the auth middleware, I will import it here[cite: 4, 5]
-// const { protect } = require('../middleware/authMiddleware'); 
+const { protect } = require('../middleware/authMiddleware');
 
 // Route configuration (REG-02, REG-04)
-// For now, testing directly without middleware. We will add "protect" later.[cite: 4]
-router.get('/search', searchRegistry);
+// For now, testing directly without middleware. I will add "protect" later.[cite: 4]
+router.get('/search', protect, searchRegistry);
 
 module.exports = router;
